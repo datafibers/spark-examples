f4d5cad1e8a57be3d649fc610de3cfc1dda67d9e
Will <willddy@gmail.com>
2019-08-31 09:19:19 -0400

    added

---
+++ src/main/scala/com/lackey/spark/examples/readwrite/OperateFile.scala
@@ -0,0 +1,20 @@
+package com.lackey.spark.examples.readwrite
+
+import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.{Dataset, SparkSession}
+
+
+object OperateFile extends App {
+  val sparkSession = SparkSession.builder().appName("simple").master("local[2]").getOrCreate()
+  sparkSession.sparkContext.setLogLevel("ERROR")
+  val spark = sparkSession
+  val filePath = "file:///Users/will/Downloads/spark_homework_will.txt"
+  val file = spark.read.format("text").load(filePath)
+  val sc = StructType.fromDDL("line STRING")
+  val frame = spark.createDataFrame(file.rdd, sc) //change schema
+  frame.show()
+  frame.createOrReplaceTempView("lines")
+  spark.sql("select line, size(split(line, ' ')) as word_cnt from lines").show
+}
+
+
